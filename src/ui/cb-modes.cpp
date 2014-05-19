6525e3c5ca5c0d9ee44556f92a39a452ebabefa8
yann.bouret <yann.bouret@gmail.com>
2014-05-19 15:17:07 +0000

    ready to create mode

---
+++ src/ui/cb-modes.cpp
@@ -0,0 +1,12 @@
+#include "convivial.h"
+
+void CreateNewMode()
+{
+    std::cerr << "Creating New Mode" << std::endl;
+    
+    //create and editable mode
+    
+    
+    //success
+    Symetry->deactivate();
+}
