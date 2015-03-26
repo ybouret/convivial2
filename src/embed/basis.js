ceb60e77f2f4aca43127585207f273e34c05d987
yann.bouret <yann.bouret@gmail.com>
2015-03-26 15:00:43 +0000

    corrected basis

--- src/embed/basis.js
+++ src/embed/basis.js
@@ -1,8 +1,8 @@
 {
     "HO"  : [ "Wave Number(cm^-1)", "Origin Shift(a.u.)" ],
-    "MOR" : [ "Wave Number(cm^-1)", "Width(a.u.)", "Depth(a.u.)" ],
-    "KRA" : [ "Wave Number(cm^-1)", "Depth(cm^-1)", "Equilibrium Position" ],
-    "TPT" : [ "Wave Number(cm^-1)", "Periodicity", "Strength" ], 
+    "MOR" : [ "Width(a.u.)", "Depth(a.u.)" ],
+    "KRA" : [ "Depth(cm^-1)", "Equilibrium Position(a.u.)\n(Mass Weighted)" ],
+    "TPT" : [ "Periodicity", "Strength" ], 
     "CHE" : [],
     "ROT" : []
 }
